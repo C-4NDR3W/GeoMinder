8ff2ac856008328200cd001e9fa81e6334f136c4
gakukek <darrylclaylayaya@gmail.com>
2024-10-23 00:17:38 +0700

    profile stuff, ~40% done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        vectorDrawables.useSupportLibrary = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -42,15 +43,13 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation ("com.google.firebase:firebase-auth-ktx:23.0.0")
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.3")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.8.3")
+    implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("com.google.android.material:material:1.9.0")
-    implementation ("androidx.fragment:fragment-ktx:1.8.4")
-
-
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
