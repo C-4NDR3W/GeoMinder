c763636f018ae0f98ad4f82d2f25ebd14d9d5e82
budijavier <alvnaltair@gmail.com>
2024-10-23 21:59:16 +0700

    navbar baru

--- app/src/main/java/com/example/geominder/MainActivity.kt
+++ app/src/main/java/com/example/geominder/MainActivity.kt
@@ -5,43 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController // Declare navController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Set up NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        var navController = navHostFragment.navController
 
         // Set up BottomNavigationView with NavController
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
 
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_add -> {
-                    navController.navigate(R.id.noteCreatorFragment)
-                    true
-                }
-                R.id.navigation_notifications -> {
-                    navController.navigate(R.id.navigation_notifications)
-                    true
-                }
-                R.id.navigation_profile -> {
-                    navController.navigate(R.id.profileFragment)
-                    true
-                }
-                else -> false
-            }
+
+        findViewById<FloatingActionButton>(R.id.fab_center).setOnClickListener {
+            // Perform navigation or any other action when the FAB is clicked
+            navController.navigate(R.id.navigation_note_creator)
         }
     }
 }
