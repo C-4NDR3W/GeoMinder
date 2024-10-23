9c682417be5a080d8acc218682351a96493a9831
gakukek <darrylclaylayaya@gmail.com>
2024-10-24 00:07:18 +0700

    touched up edit profile pic

--- app/src/main/java/com/example/geominder/ProfileFragment.kt
+++ app/src/main/java/com/example/geominder/ProfileFragment.kt
@@ -1,6 +1,10 @@
 package com.example.geominder
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +14,9 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -54,7 +61,12 @@ class ProfileFragment : Fragment() {
         }
 
         editProfilePictureButton.setOnClickListener{
-            handleEditProfilePicture()
+            if(checkCameraPermission() && checkGalleryPermission()){
+                handleEditProfilePicture()
+            } else{
+                requestCameraPermission()
+                requestGalleryPermission()
+            }
         }
 
     }
@@ -163,8 +175,76 @@ class ProfileFragment : Fragment() {
     }
 
     private fun handleEditProfilePicture(){
-
+        val options = arrayOf("Take Photo", "Choose from Gallery")
+        AlertDialog.Builder(requireContext()).setTitle("Edit Profile Picture").setItems(options){ dialog, which ->
+            when(which){
+                0 -> openCamera()
+                1 -> openGallery()
+            }
+        }.show()
     }
 
+    private fun openCamera(){
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, MEDIA_IMAGE_REQUEST_CODE)
+    }
+
+    private fun checkCameraPermission(): Boolean{
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkGalleryPermission(): Boolean{
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun requestCameraPermission(){ //reminder to check out the non deprecated version
+        requestPermissions(
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun requestGalleryPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            requestPermissions(
+                arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),
+                MEDIA_IMAGE_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            requestPermissions(
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                MEDIA_IMAGE_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+//    make on activityResult or find non-deprecated version
+
+    companion object{
+        const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+        const val CAMERA_REQUEST_CODE = 1002
+        const val MEDIA_IMAGE_REQUEST_CODE = 1003
+        const val MEDIA_IMAGE_PERMISSION_REQUEST_CODE = 1004 //cheonsa
+    }
 
 }
