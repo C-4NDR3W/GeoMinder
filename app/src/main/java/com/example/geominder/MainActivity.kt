7d67796d2c5ebf97c47afc061affee765a81ad21
Andrew <clarenceandrew9105@gmail.com>
2024-10-23 21:10:18 +0700

    Combined Notifications and Notes

--- app/src/main/java/com/example/geominder/MainActivity.kt
+++ app/src/main/java/com/example/geominder/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController // Declare navController
@@ -23,5 +25,19 @@ class MainActivity : AppCompatActivity() {
         // Set up BottomNavigationView with NavController
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_add -> {
+                    navController.navigate(R.id.noteCreatorFragment)
+                    true
+                }
+                R.id.navigation_notifications -> {
+                    navController.navigate(R.id.navigation_notifications)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
