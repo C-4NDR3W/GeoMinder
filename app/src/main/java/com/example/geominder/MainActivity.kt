b655f7a7be7d710a53464a4f68623e509a1144d4
Andrew <clarenceandrew9105@gmail.com>
2024-10-23 19:50:59 +0700

    Notes Creation and View Part 2

--- app/src/main/java/com/example/geominder/MainActivity.kt
+++ app/src/main/java/com/example/geominder/MainActivity.kt
@@ -5,14 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController // Declare navController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         // Set up NavController
