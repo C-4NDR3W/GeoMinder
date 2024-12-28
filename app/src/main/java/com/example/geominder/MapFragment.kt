1e20e29f27c742c628c375f970aa19b9e3a3de13
kevinh11 <75928654+kevinh11@users.noreply.github.com>
2024-12-28 23:27:34 +0700

    added default value for LatLng

--- app/src/main/java/com/example/geominder/MapFragment.kt
+++ app/src/main/java/com/example/geominder/MapFragment.kt
@@ -89,7 +89,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     //biar bisa dipakai untuk location bias
     private lateinit var locationLightBox: ConstraintLayout
-    private lateinit var currLatLng : LatLng
+    private var currLatLng = LatLng(0.0, 0.0)
 
     private var results: MutableList<Prediction> = mutableListOf<Prediction>()
     private lateinit var placeSuggestionAdapter: PlaceAdapter
@@ -435,7 +435,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 Log.e("MapFragment", "Failed to get last location: ${e.message}")
                 Toast.makeText(requireContext(), "Failed to get location.", Toast.LENGTH_SHORT)
                     .show()
-            }
+            }q
     }
 
 
